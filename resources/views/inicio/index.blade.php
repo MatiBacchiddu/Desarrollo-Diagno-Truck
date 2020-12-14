732e7329a2fd7db7ee2fc8f0049889d1ed5d18d0
Matias Bacchiddu <matidanibacchiddu@gmail.com>
2020-12-14 11:22:14 -0300

    Primer commit

---
+++ resources/views/inicio/index.blade.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>DiagnoTruck</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom Fonts -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+  <!-- Custom CSS -->
+  <link href="css/stylish-portfolio.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/estilos.css">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <a class="menu-toggle rounded" href="#">
+    <i class="fas fa-bars"></i>
+  </a>
+  <nav id="sidebar-wrapper">
+    <ul class="sidebar-nav navegacion">
+      <li class="sidebar-brand">
+        <a class="js-scroll-trigger" href="#page-top">DiagnoTruck</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="js-scroll-trigger" href="#page-top">Inicio</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="js-scroll-trigger" href="#about">Sobre Nosotros</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="js-scroll-trigger" href="#services">Servicios</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="js-scroll-trigger" href="#portfolio">Galeria</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="js-scroll-trigger" href="#portfolio">Trabajá con nosotros</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="js-scroll-trigger" href="#contacto">Contacto</a>
+      </li>
+    </ul>
+  </nav>
+
+  <!-- Header -->
+  <header class="contenedor-header masthead d-flex">
+    <div class="container text-center my-auto">
+        <img src="{{asset('/img/logo.png')}}" alt="imagen">
+      <h1 class="mb-1 nombre-diagno">Diagno <span>Truck</span></h1>
+      <h3 class="mb-5">
+        <p class="slogan">Centro Electrónico para Diagnóstico de Pesados</p>
+      </h3>
+      <a class="btn btn-dark btn-xl js-scroll-trigger" href="#about">Sobre nosotros</a>
+    </div>
+    <div class="overlay"></div>
+  </header>
+
+  <!-- About -->
+  <section class="content-section bg-light" id="about">
+    <div class="container text-center">
+      <div class="row contenedor-nosotros">
+        <div class="col-lg-10 mx-auto">
+          <h2 class="titulo-nosotros">Sobre nosotros</h2>
+          <p class="lead mb-5">
+
+            Somos Diagno Truck S.R.L., una empresa dedicada exclusivamente a la inyección electrónica de vehículos diesel, creada para lograr que los mismos, mantengan el excelente rendimiento para el que fueron creados.
+            <br>
+            <br>
+            Contamos con 2 talleres, uno, situado sobre la colectora del Acc. Oeste mano a capital, a la altura del km 39 y el otro taller en Av colectora este panamericana 1860 ramal escobar km 47,5
+            <br>
+            <br>
+            Nuestro personal, dispone de la más amplia gama de equipos de prueba y diagnóstico y se basa en una amplia experiencia acumulada durante años, como así también en el constante estudio de las nuevas tecnologías.
+            <br>
+            <br>
+            Entre nuestros equipos, contamos con un banco a rodillos que nos permite medir la potencia a la rueda en todo el régimen del motor, e incluso simular un viaje con una carga determinada, sin necesidad de sacar el vehículo del taller, eliminando así el riesgo que esto conlleva.
+            <br>
+            <br>
+            Como innovación al país, sumamos el centro de reprogramación, con la mayor tecnología y seguridad para nuestros camiones. Llevando el torque máximo a menos revoluciones de motor, logrando asi, mejores rendimientos y una disminución de consumo de combustible de entre 2 y 4 litros cada 100km.
+            <br>
+            <br>
+            A lo mencionado, sumamos nuestro propio laboratorio diesel, que dispone de la más amplia gama de equipos de prueba y diagnostico para la reparación y calibración de sistemas de inyección diesel.
+            <br>
+            <br>
+            Por todo esto, es que podemos brindar a nuestros clientes un servicio basado en los mas altos estándares de calidad y seguridad a la altura de las exigencias de la actualidad. Garantizando al 100% nuestros trabajos.
+            <br>
+            <br>
+            <a class="btn btn-dark btn-xl js-scroll-trigger" href="#services">Nuestros Servicios</a>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Services -->
+  <section class="contenedor-servicios content-section bg-primary text-white text-center" id="services">
+    <div class="container">
+      <div class="content-section-heading">
+        <h3 class="text-secondary mb-0">Servicios</h3>
+        <h2 class="mb-5">¿Que ofrecemos?</h2>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+          <span class="icono-amarillo service-icon rounded-circle mx-auto mb-3">
+            <i class="fas fa-truck-moving"></i>
+          </span>
+          <h4>
+            <strong>Servicio</strong>
+          </h4>
+          <p class="text-faded mb-0">Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5 mb-lg-0">
+          <span class="icono-amarillo service-icon rounded-circle mx-auto mb-3">
+            <i class="fas fa-truck-moving"></i>
+          </span>
+          <h4>
+            <strong>Servicio</strong>
+          </h4>
+          <p class="text-faded mb-0">FLorem ipsum dolor sit amet.</p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-5 mb-md-0">
+          <span class="icono-amarillo service-icon rounded-circle mx-auto mb-3">
+            <i class="fas fa-truck-moving"></i>
+          </span>
+          <h4>
+            <strong>Servicio</strong>
+          </h4>
+          <p class="text-faded mb-0">Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <span class="icono-amarillo service-icon rounded-circle mx-auto mb-3">
+            <i class="fas fa-truck-moving"></i>
+          </span>
+          <h4>
+            <strong>Servicio</strong>
+          </h4>
+          <p class="text-faded mb-0">Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- Portfolio -->
+  <section class="content-section" id="portfolio">
+    <div class="container">
+      <div class="content-section-heading text-center">
+        <h2 class="text-secondary mb-5">Galería de imágenes </h2>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6">
+          <a class="portfolio-item" href="#!">
+            <div class="caption">
+              <div class="caption-content">
+                <div class="h2">lorem ipsum1</div>
+                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptates.</p>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/camion2.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-6">
+          <a class="portfolio-item" href="#!">
+            <div class="caption">
+              <div class="caption-content">
+                <div class="h2">lorem ipsum2</div>
+                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptates.</p>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/camion2.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-6">
+          <a class="portfolio-item" href="#!">
+            <div class="caption">
+              <div class="caption-content">
+                <div class="h2">lorem ipsum3</div>
+                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptates.</p>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/camion2.jpg" alt="">
+          </a>
+        </div>
+        <div class="col-lg-6">
+          <a class="portfolio-item" href="#!">
+            <div class="caption">
+              <div class="caption-content">
+                <div class="h2">lorem ipsum4</div>
+                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, voluptates.</p>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/camion2.jpg" alt="">
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Call to Action -->
+  <section class="contenedor-contacto content-section bg-primary text-white" id="contacto">
+    <div class="contenedor container text-center">
+      <h2 class="mb-4">Ponete en contacto con nosotros</h2>
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Correo electronico</label>
+          <input type="email" placeholder="e-mail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+
+        <div class="form-group">
+            <label for="exampleInputEmail1">Teléfono</label>
+            <input type="text" placeholder="teléfono" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+
+          <div class="form-group">
+            <label for="exampleInputEmail1">Empresa</label>
+            <input type="text" placeholder="empresa" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          </div>
+
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Mensaje</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+        <button type="submit" class="btn btn-outline-warning boton-enviar mt-3">Enviar</button>
+      </form>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="contenedor-footer footer text-center">
+    <div class="container">
+
+      <ul class="list-inline mb-5">
+        <li class="list-inline-item">
+          <a class="social-diagno rounded-circle text-white mr-3" href="#!">
+            <i class="icon-social-facebook"></i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="social-diagno rounded-circle text-white mr-3" href="https://www.instagram.com/diagnotruck/" >
+            <i class="icon-social-instagram"></i>
+          </a>
+        </li>
+      </ul>
+      <p class="text-muted small mb-0">Copyright &copy; Diagno Truck 2020</p>
+    </div>
+  </footer>
+
+  <!-- Scroll to Top Button-->
+  <div class="icono-ws">
+    <a class="scroll-to-top rounded js-scroll-trigger" href="#page-top">
+      <i class="fab fa-whatsapp"></i>
+    </a>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/stylish-portfolio.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+</body>
+
+</html>

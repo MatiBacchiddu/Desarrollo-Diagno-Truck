2790c93f122913194535319e82839e4a29c471c1
Matias Bacchiddu <matidanibacchiddu@gmail.com>
2021-01-12 12:28:55 -0300

    cambio pequeño

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -11,9 +11,7 @@ class InicioController extends Controller
     //
     public function index()
     {
-
         $noticias = Noticia::all();
-
         return view('inicio.index')->with('noticias', $noticias);
     }
 }
